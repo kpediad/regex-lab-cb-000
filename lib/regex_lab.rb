ba30d96776da377b884417c7574df22fb9eb1652
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-14 14:27:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  return true if word.match(/[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  return text.scan(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  return true if text.match(/^[A-Z]\w+$[.!?]/)
 end
 
 def valid_phone_number?(phone)
-
+  return true if phone.match(/^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/)
 end
