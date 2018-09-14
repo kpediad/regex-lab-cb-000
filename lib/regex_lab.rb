ad72e74f1cab0819ca8edaecf449b028f248d7f3
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-14 14:45:42 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,6 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiou]/i) return true end
+  return true if word.match(/^[aeiou]/i)
+  false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
